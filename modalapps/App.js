2514130971103551b0d4adcc47ca09f11d25b9be
Permata Nur <mata@Permatas-MacBook-Pro.local>
2020-04-09 15:40:14 +0700

    add modals app

--- modalapps/App.js
+++ modalapps/App.js
@@ -1,98 +1,137 @@
 import React, {Component} from 'react';
 import {
-  Alert,
-  Modal,
+  SafeAreaView,
+  View,
+  FlatList,
   StyleSheet,
   Text,
-  TouchableHighlight,
-  View,
+  TouchableOpacity,
+  Modal,
+  Image,
+  Dimensions,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-Icon.loadFont()
-const myIcon = <Icon name="cancel" size={30} color="red" />;
 
-class App extends Component {
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Park SHin hye',
+    info: 'ini adalah first item detail',
+    url:
+      'https://media.matamata.com/thumbs/2018/11/09/45815-park-shin-hye-di-drama-memories-of-the-alhambra-soompi/745x489-img-45815-park-shin-hye-di-drama-memories-of-the-alhambra-soompi.jpg',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Song Hye Ko',
+    info: 'ini adalah first item detail',
+    url:
+      'https://img-k.okeinfo.net/content/2020/04/01/33/2192278/mengenal-lebih-dalam-sosok-song-hye-kyo-jpdpM8V3sT.jpg',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Park Ji Sung',
+    info: 'ini adalah first item detail',
+    url:
+      'https://cdn2.tstatic.net/jabar/foto/bank/images/park-ji-sung_20180112_161037.jpg',
+  },
+];
+
+function Item({title, onPress}) {
+  return (
+    <TouchableOpacity onPress={() => onPress()}>
+      <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+export default class App extends Component {
   state = {
-    modalVisible: false,
-  };
-
-  setModalVisible = visible => {
-    this.setState({modalVisible: visible});
+    is_visible_modal: false,
+    title: '',
+    info: '',
+    url: '',
   };
 
   render() {
-    const {modalVisible} = this.state;
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={({item}) => (
+            <Item
+              onPress={() =>
+                this.setState({
+                  is_visible_modal: true,
+                  title: item.title,
+                  info: item.info,
+                  url: item.url,
+                })
+              }
+              title={item.title}
+            />
+          )}
+          keyExtractor={item => item.id}
+        />
         <Modal
           animationType="slide"
           transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-          }}>
-          <View style={styles.centeredView}>
-            <Text> INi adalah modal</Text>
-            <TouchableHighlight
-              style={styles.openButton}
-              onPress={() => {
-                this.setModalVisible(!this.state.modalVisible);
-              }}>
-              {myIcon}
-            </TouchableHighlight>
+          visible={this.state.is_visible_modal}
+          onRequestClose={data => this.setState({is_visible_modal: true})}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'white',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{fontWeight: 'bold', fontSize: 18}}>
+              {this.state.title}
+            </Text>
+            <Image
+              style={{width: windowWidth * 0.8, height: (windowHeight * 2) / 3}}
+              source={{
+                uri: this.state.url,
+              }}
+            />
+            <Text>{this.state.info}</Text>
+            <TouchableOpacity
+              onPress={() => this.setState({is_visible_modal: false})}>
+              <View
+                style={{
+                  marginTop: 40,
+                  width: 80,
+                  height: 30,
+                  backgroundColor: '#909090',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 10,
+                }}>
+                <Text style={{color: 'white'}}>Kembali</Text>
+              </View>
+            </TouchableOpacity>
           </View>
         </Modal>
-
-        <TouchableHighlight
-          style={styles.openButton}
-          onPress={() => {
-            this.setModalVisible(true);
-          }}>
-          <Text style={styles.textStyle}>Show Modal</Text>
-        </TouchableHighlight>
-      </View>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  centeredView: {
+  container: {
     flex: 1,
-    backgroundColor: 'rgba(52,52,52,0.5)',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
+    marginTop: 30,
   },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  openButton: {
-    backgroundColor: '#F194FF',
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
+  title: {
+    fontSize: 32,
   },
 });
-
-export default App;
